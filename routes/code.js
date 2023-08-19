824fe131edf821e5ce2c24d7194a87872d024d35
Amchia_Mohamed <amchiamohamed41@gmail.com>
2023-08-19 23:21:30 +0100

    fix bugs

--- routes/code.js
+++ routes/code.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bodyparser = require("body-parser");
+
 require("dotenv").config();
 
 const { Configuration, OpenAIApi } = require("openai");
--- routes/image.js
+++ routes/image.js
@@ -1,24 +1,30 @@
-// routes/openai.js
-
 const express = require('express');
 const { Configuration, OpenAIApi } = require('openai');
 
 const router = express.Router();
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+
+// Body-parser middleware
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+// Validate OPENAI_API_KEY environment variable
+if (!configuration.apiKey) {
+  throw new Error('OPENAI_API_KEY environment variable is required');
+}
+
 const openai = new OpenAIApi(configuration);
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async ({ body: { prompt, amount = 1, resolution = "512x512" } }, res) => {
   try {
-    const { prompt, amount = 1, resolution = "512x512" } = req.body;
-
     // Validate request body parameters before proceeding
     if (!prompt) {
       return res.status(400).json({ error: 'Prompt is required' });
@@ -35,7 +41,7 @@ router.post('/', async (req, res) => {
     res.status(200).json({ photo: imageUrl });
   } catch (error) {
     console.error("The error is", error);
-    res.status(500).send(error?.response?.data?.error?.message || 'Something went wrong');
+    res.status(500).send('Something went wrong');
   }
 });
 
